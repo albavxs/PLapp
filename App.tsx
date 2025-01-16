35e7dcffe33229f4765d60f5bab5b635fc39bda0
ViniJosee <contato.viniciusjose2002@gmail.com>
2025-01-16 19:16:05 -0300

    app.tsx att

--- App.tsx
+++ App.tsx
@@ -2,11 +2,9 @@ import React, { useEffect, useState } from "react";
 import { SafeAreaView, StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import SplashScreen from "./src/pages/SplashScreen";
 import HomeScreen from "./src/pages/HomeScreen";
 import CatalogScreen from "./src/pages/CatalogScreenHair";
-import Icon from "react-native-vector-icons/MaterialIcons";
 
 // Tipos das rotas
 export type RootStackParamList = {
@@ -14,35 +12,8 @@ export type RootStackParamList = {
   CatalogScreen: undefined;
 };
 
-// Criação dos navegadores
+// Criação do navegador de pilha
 const Stack = createStackNavigator<RootStackParamList>();
-const Tab = createBottomTabNavigator();
-
-// Componente de Navegação por Abas
-const TabNavigator = () => (
-  <Tab.Navigator
-    screenOptions={({ route }) => ({
-      tabBarIcon: ({ color, size }) => {
-        let iconName: string;
-
-        if (route.name === "Home") {
-          iconName = "home";
-        } else if (route.name === "Catalog") {
-          iconName = "style"; // Ícone relacionado a catálogo
-        }
-
-        // Renderiza o componente Icon corretamente
-        return <Icon name={iconName} size={size} color={color} />;
-      },
-      tabBarActiveTintColor: "tomato",
-      tabBarInactiveTintColor: "gray",
-      headerShown: false,
-    })}
-  >
-    <Tab.Screen name="Home" component={HomeScreen} />
-    <Tab.Screen name="Catalog" component={CatalogScreen} />
-  </Tab.Navigator>
-);
 
 // Componente Principal do App
 const App = () => {
@@ -64,10 +35,9 @@ const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        {/* Substituí HomeScreen por TabNavigator */}
         <Stack.Screen
           name="HomeScreen"
-          component={TabNavigator}
+          component={HomeScreen}
           options={{ headerShown: false }}
         />
         <Stack.Screen
